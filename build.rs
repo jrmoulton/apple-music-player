6e8b198f65d051cdeadfb6bb08de6213bf527f1f
Jared Moulton <jaredmoulton3@gmail.com>
2021-12-21 20:08:10 -0700

    First commit

---
+++ build.rs
@@ -0,0 +1,4 @@
+
+fn main() {
+    sixtyfps_build::compile("ui/main.60").unwrap();
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+sixtyfps::include_modules!();
+
+fn main() {
+    let main_window = MainWindow::new();
+    main_window.run();
+}
